6d558ca4af144f7b010a98a99c870a5d586aa1f7
Noah Prail <nprail@filiosoft.com>
2018-06-08 12:24:56 -0400

    send slack message on startup

--- config/slack.js
+++ config/slack.js
@@ -6,19 +6,19 @@ module.exports = (app, config) => {
     config.slack.verificationToken
   )
   const web = new WebClient(config.slack.token)
+  web.chat
+    .postMessage({ channel: 'C542VDK62', text: 'Starting up...' })
+    .then(res => {
+      // `res` contains information about the posted message
+      console.log('Message sent: ', res.ts)
+    })
+    .catch(console.error)
 
   app.use('/slack/events', slackEvents.expressMiddleware())
 
   // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
   slackEvents.on('message', event => {
     console.log(event)
-    // web.chat
-    //   .postMessage({ channel: event.channel, text: 'Hello there' })
-    //   .then(res => {
-    //     // `res` contains information about the posted message
-    //     console.log('Message sent: ', res.ts)
-    //   })
-    //   .catch(console.error)
   })
 
   // Handle errors (see `errorCodes` export)
