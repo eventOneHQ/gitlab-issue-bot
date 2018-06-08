0ed58ba7f0517f76aae72776f85e877f34bfd811
Noah Prail <nprail@filiosoft.com>
2018-06-08 12:27:55 -0400

    respond to all messages BUT bot messages

--- config/slack.js
+++ config/slack.js
@@ -6,19 +6,23 @@ module.exports = (app, config) => {
     config.slack.verificationToken
   )
   const web = new WebClient(config.slack.token)
-  web.chat
-    .postMessage({ channel: 'C542VDK62', text: 'Starting up...' })
-    .then(res => {
-      // `res` contains information about the posted message
-      console.log('Message sent: ', res.ts)
-    })
-    .catch(console.error)
 
   app.use('/slack/events', slackEvents.expressMiddleware())
 
   // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
   slackEvents.on('message', event => {
+    // don't respond to other bots (e.g self)
+    if (event.subtype === 'bot_message') {
+      return
+    }
     console.log(event)
+    web.chat
+      .postMessage({ channel: event.channel, text: 'Hello there' })
+      .then(res => {
+        // `res` contains information about the posted message
+        console.log('Message sent: ', res.ts)
+      })
+      .catch(console.error)
   })
 
   // Handle errors (see `errorCodes` export)
