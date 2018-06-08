49ea3623532663bbdb9f78f52c64b6e637bddba2
Noah Prail <nprail@filiosoft.com>
2018-06-08 11:45:40 -0400

    pass token to slack

--- config/config.js
+++ config/config.js
@@ -8,7 +8,8 @@ const config = {
   baseUrl: process.env.BASE_URL || 'http://localhost:3000',
   dbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/glib',
   slack: {
-    verificationToken: process.env.SLACK_VERIFICATION_TOKEN
+    verificationToken: process.env.SLACK_VERIFICATION_TOKEN,
+    token: process.env.SLACK_TOKEN
   }
 }
 
